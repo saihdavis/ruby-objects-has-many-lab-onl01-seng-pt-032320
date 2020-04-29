1c2c62ee4a85e4d6e4e3cdd3d69c8db30c4c593e
Saida Davis <saihdavis@gmail.com>
2020-04-29 16:32:27 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Artist
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+   def songs
+    @songs 
+  end
+  
+  def add_song(song)
+    #binding.pry
+    @songs << song
+    song.artist = self
+  end
+  
+  def add_song_by_name(song_name)
+    
+    new_song = Song.new(song_name)
+    new_song.artist = self
+  end
+end 
